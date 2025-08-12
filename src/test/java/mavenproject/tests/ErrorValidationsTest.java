0d30b375c153c297dac6dd383dac0fe68460e0d0
Karan <karan.sagale@gmail.com>
2025-08-13 00:36:00 +0530

    Updated Error Validation test file

--- src/test/java/mavenproject/tests/ErrorValidationsTest.java
+++ src/test/java/mavenproject/tests/ErrorValidationsTest.java
@@ -14,6 +14,7 @@ import mavenproject.pageobjects.ProductCatalogue;
 
 public class ErrorValidationsTest extends BaseTest {
 
+        // CICD check 
 	// public static void main(String[] args) throws InterruptedException {
 
 	@Test(groups = { "ErrorHandling" }, retryAnalyzer = Retry.class)
